a11f32ebbfe2c982b78751baeb6ecdb88b4fe848
nagaken <a.com>
2026-04-19 16:40:00 +0900

    v0.0

--- azbil_to_keyence.js
+++ azbil_to_keyence.js
@@ -1,18 +1,24 @@
 "use strict";
-var az_p = 0.1;
-var az_i = 0;
-var az_d = 0;
-var key_p = 0;
-var key_i = 0;
-var key_d = 0;
-var pv_temp_range = 500;
-var mv_range = 100;
-key_p = Math.round((mv_range * 100000) / (pv_temp_range * az_p));
-if (az_i == 0) {
-    key_i = 30001;
-}
-else {
-    key_i = Math.round(az_i * 10);
-}
-key_d = Math.round(az_d * 100);
-console.log("P:" + key_p + ", " + "I:" + key_i + ", " + "D:" + ", " + key_d);
+var btn = document.getElementById("btn");
+btn.onclick = function () {
+    var az_p = parseFloat(document.getElementById("in_az_p").value);
+    var az_i = parseFloat(document.getElementById("in_az_i").value);
+    var az_d = parseFloat(document.getElementById("in_az_d").value);
+    var pv_temp_range = parseFloat(document.getElementById("in_pv_range").value);
+    var mv_range = parseFloat(document.getElementById("in_mv_range").value);
+    var key_p = 0;
+    var key_i = 0;
+    var key_d = 0;
+    key_p = Math.round((mv_range * 100000) / (pv_temp_range * az_p));
+    if (az_i == 0) {
+        key_i = 30001;
+    }
+    else {
+        key_i = Math.round(az_i * 10);
+    }
+    key_d = Math.round(az_d * 100);
+    /*console.log("P:" + key_p + ", " + "I:" + key_i + ", " + "D:" + ", " + key_d) テスト用*/
+    document.getElementById("plc_p").innerText = key_p.toString();
+    document.getElementById("plc_i").innerText = key_i.toString();
+    document.getElementById("plc_d").innerText = key_d.toString();
+};
--- azbil_to_keyence.ts
+++ azbil_to_keyence.ts
@@ -1,21 +1,30 @@
 
-var az_p :number = 0;
-var az_i :number = 0;
-var az_d :number = 0;
-var key_p :number = 0;
-var key_i :number = 0;
-var key_d :number = 0;
-var pv_temp_range :number = 0;
-var mv_range :number = 0;
+var btn = document.getElementById("btn") as HTMLElement;
+btn.onclick = function() {
+  
+  var az_p :number = parseFloat((document.getElementById("in_az_p") as HTMLInputElement).value);
+  var az_i :number = parseFloat((document.getElementById("in_az_i") as HTMLInputElement).value);
+  var az_d :number = parseFloat((document.getElementById("in_az_d") as HTMLInputElement).value);
+  var pv_temp_range :number = parseFloat((document.getElementById("in_pv_range") as HTMLInputElement).value);
+  var mv_range :number = parseFloat((document.getElementById("in_mv_range") as HTMLInputElement).value);
+  var key_p :number = 0;
+  var key_i :number = 0;
+  var key_d :number = 0;
 
-key_p = Math.round((mv_range * 100000) / (pv_temp_range * az_p));
+  key_p = Math.round((mv_range * 100000) / (pv_temp_range * az_p));
 
-if (az_i == 0) {
-  key_i = 30001;
-}else{
-  key_i = Math.round(az_i * 10); 
-}
+  if (az_i == 0) {
+    key_i = 30001;
+  }else{
+    key_i = Math.round(az_i * 10); 
+  }
 
-key_d = Math.round(az_d * 100);
+  key_d = Math.round(az_d * 100);
 
 /*console.log("P:" + key_p + ", " + "I:" + key_i + ", " + "D:" + ", " + key_d) テスト用*/
+
+  (document.getElementById("plc_p") as HTMLElement).innerText = key_p.toString();
+  (document.getElementById("plc_i") as HTMLElement).innerText = key_i.toString();
+  (document.getElementById("plc_d") as HTMLElement).innerText = key_d.toString();
+
+};
